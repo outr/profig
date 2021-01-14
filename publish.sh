002f03701d04e1687cb176ce29cf67b424f8d283
Matt Hicks <matt@courio.com>
2021-01-14 09:00:23 -0800

    Release 3.1.0

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt +clean +test +macrosJS/publishSigned +macrosJVM/publishSigned +coreJS/publishSigned +coreJVM/publishSigned +xml/publishSigned +hocon/publishSigned +yaml/publishSigned +inputJS/publishSigned +inputJVM/publishSigned +live/publishSigned +all/publishSigned sonatypeRelease
+sbt +clean +test +coreJS/publishSigned +coreJVM/publishSigned +xml/publishSigned +hocon/publishSigned +yaml/publishSigned +all/publishSigned sonatypeRelease
