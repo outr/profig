0f6f7e0ac575e69ca5e7efc912061f058abe7824
Matt Hicks <matt@courio.com>
2021-03-03 08:31:24 -0800

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt +clean +test +coreJS/publishSigned +coreJVM/publishSigned +xml/publishSigned +hocon/publishSigned +yaml/publishSigned +all/publishSigned sonatypeRelease
+sbt +clean +test +coreJS/publishSigned +coreJVM/publishSigned sonatypeRelease
